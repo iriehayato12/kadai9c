72b8066b68d936ce1533622b0738d03eaf7c415e
iriehayato <143862129+iriehayato12@users.noreply.github.com>
2023-10-04 04:29:30 +0000

    kadai9c

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,8 @@ Rails.application.routes.draw do
   resources :chats, only: [:show, :create]
   resources :groups, only: [:new, :index, :show, :edit, :create, :update, :destroy] do
    get "join" => "groups#join"
+   get "new/mail" => "groups#new_mail"
+   get "send/mail" => "groups#send_mail"
   end
   
 
